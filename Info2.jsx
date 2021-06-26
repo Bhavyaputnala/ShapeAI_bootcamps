b7ad2fdd61df1e8adc917384339f9720f211332f
Bhavyaputnala <86529007+Bhavyaputnala@users.noreply.github.com>
2021-06-26 22:05:55 +0530

    Add files via upload

---
+++ Info2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info2() {
+  return (
+    <div className="note">
+      <p>7 Days, Certified, Free Bootcamp in collaboration with
+Google Developer Groups,  Microsoft Learn Student Ambassador Program, and AWS Students Club.</p>
+    </div>
+  );
+}
+export default Info2;
